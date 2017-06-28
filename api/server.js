59f4c10ccfe29761e44abc08921f80daef2dfe5f
Milton Quirino <miltonquirino.br@gmail.com>
2017-06-28 11:20:36 -0300

    add comentario em nas fotos

--- api/server.js
+++ api/server.js
@@ -10,6 +10,16 @@ app.use(bodyParser.urlencoded({ extend: true }));
 app.use(bodyParser.json());
 app.use(multiparty());
 
+app.use(function (req, res, next) {
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  next();
+});
+
 var port = 8686;
 
 app.listen(port);
@@ -28,16 +38,14 @@ app.get('/', function (req, res) {
 
 app.post('/api', function (req, res) {
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
   var date = new Date();
   time_stamp = date.getTime();
 
-  var url_imagem = time_stamp+'_'+ req.files.arquivo.originalFilename;
+  var url_imagem = time_stamp + '_' + req.files.arquivo.originalFilename;
 
   var path_origem = req.files.arquivo.path;
   var path_destino = './uploads/' + url_imagem;
-  
+
 
   fs.rename(path_origem, path_destino, function (err) {
     if (err) {
@@ -70,9 +78,7 @@ app.post('/api', function (req, res) {
 });
 
 app.get('/api', function (req, res) {
-  
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  
+
   db.open(function (erro, mongoClient) {
     mongoClient.collection('postagens', function (err, collection) {
       collection.find().toArray(function (err, results) {
@@ -108,16 +114,16 @@ app.get('/api/:id', function (req, res) {
 });
 
 app.get('/imagens/:imagem', function (req, res) {
- console.log('imagesss');
+
   var img = req.params.imagem;
 
-  fs.readFile('./uploads/'+img, function(err, conteudo){
-    if(err){
+  fs.readFile('./uploads/' + img, function (err, conteudo) {
+    if (err) {
       res.status(400).json(err);
       return;
     }
     res.writeHead(200, {
-      'content-type':'image/jpg'
+      'content-type': 'image/jpg'
     });
     res.end(conteudo);
 
@@ -125,12 +131,20 @@ app.get('/imagens/:imagem', function (req, res) {
 });
 
 app.put('/api/:id', function (req, res) {
-
+  // res.send("putss : "+ req.params.id + " comentario : "+ req.body.comentario);
   db.open(function (erro, mongoClient) {
+
     mongoClient.collection('postagens', function (err, collection) {
       collection.update(
         { _id: objectId(req.params.id) },
-        { $set: { titulo: req.body.titulo } },
+        { $push: 
+            { 
+              comentarios: {
+                id_comentario : new objectId(),
+                comentario: req.body.comentario
+              }
+            } 
+          },
         {},
         function (erro, records) {
           if (erro) {
@@ -144,6 +158,7 @@ app.put('/api/:id', function (req, res) {
 
     });
   });
+
 });
 
 app.delete('/api/:id', function (req, res) {
